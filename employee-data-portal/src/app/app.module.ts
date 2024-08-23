c889c2db3c1c3d8733de6b15bf352782ef847ab8
Sindhu M <Sindhu.M@godigit.com>
2024-08-23 12:45:36 +0530

    Implemented employee portal

---
+++ employee-data-portal/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { HttpService } from './services/http.service';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeecardComponent } from './components/employeecard/employeecard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    DashboardComponent,
+    HeaderComponent,
+    EmployeecardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ employee-data-portal/src/app/components/signup/signup.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+  isAdmin: boolean = false;
+
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
+    this.signupForm = this.fb.group({
+      name: ['', [Validators.required]],
+      dob: ['', [Validators.required]],
+      mobile: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      role: ['user', [Validators.required]], // Default role is 'user'
+      department: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void { }
+
+  toggleRole(): void {
+    this.isAdmin = !this.isAdmin;
+    this.signupForm.patchValue({ role: this.isAdmin ? 'admin' : 'user' });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      const formData = this.signupForm.value;
+      if (formData.role === 'admin') {
+        formData.department = 'admin';
+        this.http.post('http://localhost:3000/admin', formData).subscribe({
+          next: response => {
+            console.log('Signup successful', response);
+            this.router.navigate(['/dashboard']);
+          },
+          error: error => {
+            console.error('Signup failed', error);
+          }
+        });
+      } else {
+        this.http.post('http://localhost:3000/user', formData)
+          .subscribe({
+            next: response => {
+              console.log('Signup successful', response);
+              this.router.navigate(['/dashboard']);
+
+              //signup  successfull
+            },
+            error: error => {
+              console.error('Signup failed', error);
+              //signup error
+            }
+          });
+      }
+    } else {
+      console.log('Form is invalid');
+      return;
+    }
+  }
+}
